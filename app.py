fd7707567aba4566663bb9d35e31ca306fa79120
cristianpb <felipebrokate@gmail.com>
2019-04-13 00:01:35 +0200

    Remove mmdetection dependency

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask, render_template, Response, render_template_string, send_from_directory, request
-from flask_cors import CORS, cross_origin
-import json
 import os
-import  cv2
-from PIL import Image
+import json
+import cv2
 import numpy as np
-import mmcv
-from mmcv.runner import load_checkpoint
-
+from PIL import Image
+from flask import Flask, render_template, send_from_directory, request
+from flask_cors import CORS, cross_origin
 import torch
 import torch.nn as nn
-
-from mmdet.models import build_detector
-from mmdet.apis import inference_detector
-from mmdet.core import get_classes
-from mmcv.visualization.color import color_val
-
 import torchvision.models as models
 import torchvision.transforms as transforms
 import torch.backends.cudnn as cudnn
-
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -31,11 +18,13 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # DETECTION OPENCV
 cvNet = cv2.dnn.readNetFromTensorflow(
-        '/model/faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb',
-        '/model/faster_rcnn_resnet50_coco_2018_01_28.pbtxt')
+        '/model/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb',
+        '/model/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+        #'/model/faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb',
+        #'/model/faster_rcnn_resnet50_coco_2018_01_28.pbtxt')
 
 COLORS = np.random.uniform(0, 255, size=(100, 3))
-CLASS_NAMES = get_classes('coco')
+#CLASS_NAMES = get_classes('coco')
 
 # Get label
 filename = os.path.join('/model/resnet18-100', 'idx_to_class.json')
@@ -77,9 +66,7 @@ class Crop(object):
 crop = Crop()
 
 def filter_prediction(result, width, height):
-    print("verf",result.shape)
     result = result[result[:,2] > 0.4] # Filter by score
-    print("after", result.shape)
 
     dist_box = 99999
     select_box = None
@@ -134,9 +121,7 @@ def api_object_detection():
         cvNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
         cvOut = cvNet.forward()
         result = cvOut[0,0,:,:]
-        print("shape", result.shape)
         result = result[result[:,2] > 0.4] # Filter by score
-        print("after", result.shape)
 
         res = list()
         for i, detection in enumerate(cvOut[0,0,:,:]):
@@ -148,45 +133,12 @@ def api_object_detection():
                 y2 = detection[6] * height
                 res.append({
                     'bbox': [x1, y1, x2-x1, y2-y1],
-                    'class': "score: {:.2f} class {}".format(detection[2], 
-                        CLASS_NAMES[int(detection[1])]),
+                    'class': "score: {:.2f} class {}".format(detection[2], int(detection[1])),
                     'prob': float(detection[2])})
 
         return json.dumps(res)
     else:
         return json.dumps({'status': 'no image'})
-
-# curl -F image=@test.jpg http://localhost:5000/object_detection
-@app.route('/object_detection',methods=['POST'])
-def object_detection():
-    # recieve image in files
-    image = request.files.get('image', None)
-    if image:
-        # decode istringmage
-        nparr = np.fromstring(image.read(), np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        height, width = img.shape[:-1]
-
-        # detect boxes
-        result = inference_detector(detection_model, img, cfg, device=device)
-
-        # Parse results
-        for i, class_box in enumerate(result):
-            class_box = class_box[class_box[:,4] > 0.4] # Filter by score
-            for box in class_box:
-                x1, y1 = (int(box[0]), int(box[1]))
-                x2, y2 = (int(box[2]), int(box[3]))
-                cv2.rectangle(img, (x1, y1), (x2,y2), COLORS[i], 2)
-                cv2.putText(img, "{}, {:.2f}".format(CLASS_NAMES[i], box[4]),
-                        (int(x1 + 0.005*width), int(y1+0.03*height)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                        COLORS[i],
-                        2)
-        _, img_encoded = cv2.imencode('.jpg', img)
-        return Response(img_encoded.tobytes(), mimetype='image/jpeg')
-    else:
-        return json.dumps({"sta": 'ok'})
 
 @app.route('/api/predict',methods=['POST'])
 def api_predict():
@@ -241,96 +193,6 @@ def api_predict():
 
     else:
         return json.dumps({'status': 'no box'})
-
-# curl -F image=@test.jpg http://localhost:5000/predict
-@app.route('/predict',methods=['POST'])
-def predict():
-
-    # decode image
-    image = request.files["image"]
-    nparr = np.fromstring(image.read(), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    height, width = img.shape[:-1]
-
-    # Detect all boxes with retina100
-    result = inference_detector(detection_model, img, cfg, device=device)
-
-
-    select_box = filter_prediction(result, width, height)
-
-    result = result[2] # Get class car
-    result = result[result[:,4] > 0.4] # Filter by score
-
-    for box in result:
-        x1, y1 = (int(box[0]), int(box[1]))
-        x2, y2 = (int(box[2]), int(box[3]))
-        cv2.rectangle(img, (x1, y1), (x2,y2), color_val('blue'), 2)
-        cv2.putText(img, "{}, {:.2f}".format(CLASS_NAMES[2], box[4]),
-                (int(x1 + 0.005*width), int(y1+0.03*height)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                color_val('blue'),
-                2)
-
-    # Selected box
-    if select_box is not None:
-        box = select_box
-        x1, y1 = (int(box[0]), int(box[1]))
-        x2, y2 = (int(box[2]), int(box[3]))
-        # Crop and resize
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-
-        preprocess = transforms.Compose([
-            crop,
-            transforms.Resize([224,224]),
-            transforms.ToTensor(),
-            normalize,
-        ])
-
-        args = (Image.fromarray(img), (x1,y1,x2,y2))
-        sample = preprocess(args)
-        sample = sample.unsqueeze(0)
-
-        sample = sample.cuda(device)
-
-        # Inference classification model
-        output = classification_model(sample)
-        softmax = nn.Softmax()
-        norm_output = softmax(output)
-
-        # Get max probability
-        probs, preds = norm_output.topk(5, 1, True, True)
-        pred = preds.data.cpu().tolist()[0]
-        prob = probs.data.cpu().tolist()[0]
-
-        cv2.rectangle(img, (x1, y1), (x2,y2), color_val('green'), 2)
-
-        #Creates two subplots and unpacks the output array immediately
-        fig = plt.figure()
-        ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3, rowspan=2)
-        ax1.imshow(img)
-        ax1.axis('off')
-        ax2 = plt.subplot2grid((3, 3), (2, 1))
-        ax2.barh(range(len(pred[:5])), prob[:5], color='blue')
-        ax2.set_yticks(range(len(pred[:5])))
-        ax2.set_yticklabels([all_categories[str(x)] for x in pred[:5]])
-        ax2.invert_yaxis()  # labels read top-to-bottom
-        fig.tight_layout(pad=0)
-        fig.canvas.draw()
-
-        # Now we can save it to a numpy array.
-        data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
-        _, img_encoded = cv2.imencode('.jpg', data)
-
-        return Response(img_encoded.tobytes(), mimetype='image/jpeg')
-    else:
-        _, img_encoded = cv2.imencode('.jpg', img)
-        return Response(img_encoded.tobytes(), mimetype='image/jpeg')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
