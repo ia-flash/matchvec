b3c2fce481ec5dba01e8e27ed155fbf047a93f4b
cristianpb <felipebrokate@gmail.com>
2019-07-31 15:31:49 +0200

    WIP: sam integration variables

--- matchvec/lambda_function.py
+++ matchvec/lambda_function.py
@@ -5,10 +5,10 @@ import cv2
 import onnxruntime
 from PIL import Image
 import base64
-from process import predict_class, predict_objects
+#from process import predict_class, predict_objects
 
-#if path.isfile('/tmp/classification_model.onnx') != True:
-if path.isfile('/tmp/classification_model.onnx') != True and False:
+#if path.isfile('/tmp/classification_model.onnx') != True and False:
+if path.isfile('/tmp/classification_model.onnx') != True:
     import boto3
     print("Downloading..")
     s3 = boto3.resource('s3')
@@ -17,24 +17,27 @@ if path.isfile('/tmp/classification_model.onnx') != True and False:
     print("Downloading ok")
 
 def lambda_handler(event, context):
+    print("ENV", getenv('BACKEND'))
+    print("ENV", getenv('DETECTION_THRESHOLD'))
+    print("LISTDIR", listdir('/tmp'))
 
     res = list()
     data = event.get('body', None)
-    if data:
-        #  read encoded image
-        imageString = base64.b64decode(data)
+    #if data:
+    #    #  read encoded image
+    #    imageString = base64.b64decode(data)
 
-        #  convert binary data to numpy array
-        nparr = np.frombuffer(imageString, np.uint8)
+    #    #  convert binary data to numpy array
+    #    nparr = np.frombuffer(imageString, np.uint8)
 
-        #  let opencv decode image to correct format
-        img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR);
+    #    #  let opencv decode image to correct format
+    #    img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR);
 
-        print("IMAGE", img)
+    #    print("IMAGE", img)
 
-        res.append(predict_class(img))
+    #    res.append(predict_class(img))
 
     return {
         'statusCode': 200,
-        'body': json.dumps(res)
+        'body': 'hello' #json.dumps(res)
     }
