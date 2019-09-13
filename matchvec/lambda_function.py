799642a1527fc1db71f62a3f1c38425634d1b7a4
cristianpb <felipebrokate@gmail.com>
2019-09-13 22:56:07 +0200

    WIP: parse body in lambda_function

--- matchvec/lambda_function.py
+++ matchvec/lambda_function.py
@@ -5,26 +5,10 @@ import cv2
 import onnxruntime
 from PIL import Image
 import base64
-#from process import predict_class, predict_objects
-<<<<<<< HEAD
-=======
-
 from cgi import parse_header, parse_multipart
 from io import BytesIO
->>>>>>> 5e242d3... WIP: add test get route
+from process import predict_class, predict_objects
 
-<<<<<<< HEAD
-#if path.isfile('/tmp/classification_model.onnx') != True and False:
-if path.isfile('/tmp/classification_model.onnx') != True:
-    import boto3
-    print("Downloading..")
-    s3 = boto3.resource('s3')
-    myobject = s3.Object('iaflash', 'classifcation_model.onnx')
-    myobject.download_file('/tmp/classifcation_model.onnx')
-    print("Downloading ok")
-
-=======
->>>>>>> 06cd91b... Fix errors for classification and detection
 def lambda_handler(event, context):
     print("ENV", getenv('BACKEND'))
     print("ENV", getenv('DETECTION_THRESHOLD'))
@@ -43,39 +27,26 @@ def lambda_handler_classification(event, context):
     print("LISTDIR", listdir('/tmp'))
 
     res = list()
-    data = event.get('body', None)
+    body_str = event.get('body', None)
+    body_parsed = json.loads(body_str).get('body', None)
 
-    c_type, c_data = parse_header(event['headers']['Content-Type'])
-    assert c_type == 'multipart/form-data'
-    decoded_string = base64.b64decode(event['body'])
-    form_data = parse_multipart(BytesIO(decoded_string), c_data)
+    #c_type, c_data = parse_header(event['headers']['Content-Type'])
+    #assert c_type == 'multipart/form-data'
+    #decoded_string = base64.b64decode(event['body'])
+    #form_data = parse_multipart(BytesIO(decoded_string), c_data)
 
-    #print(data)
-    #post_data = base64.b64decode(event['body'])
-    #headers, data_2 = post_data.split('\r\n', 1)
-
-    if data:
-        print(type(data))
-        print(data)
-
-
-        #print(post_data)
-        #print(headers)
-        #print(data_2)
-
-
-        #nparr = np.frombuffer(data.read(), np.uint8)
-        #img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        #img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
-        #print(img.shape)
+    if body_parsed:
+        print(type(body_parsed))
+        print(body_parsed[:100])
 
         #  read encoded image
-        #imageString = base64.b64decode(data)
-        ##  convert binary data to numpy array
-        #nparr = np.frombuffer(imageString, np.uint8)
-        ##  let opencv decode image to correct format
-        #img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
-        #res.append(predict_class(img))
+        imageString = base64.b64decode(body_parsed)
+        #  convert binary data to numpy array
+        nparr = np.frombuffer(imageString, np.uint8)
+        #  let opencv decode image to correct format
+        img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+
+        res.append(predict_class(img))
 
     return {
         'statusCode': 200,
