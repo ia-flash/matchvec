779e44ada26bf5ecbf3ee279ecadbabb77fad6f2
victorjourne <journe.victor@gmail.com>
2021-02-04 16:19:57 +0100

    WP: sagger template with prio

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,6 +11,11 @@ def assert_clio(resp_data) :
     assert resp_data[0][0]["brand_model_classif"]["pred"][0] == "RENAULT CLIO",  'Not a clio'
     assert resp_data[0][0]["confidence"] > 0.8, "Car not detected"
 
+def assert_pompier(resp_data) :
+    assert resp_data[0][0]["prio_classif"]["prob"][0] > 0.7, 'Classif confidence too low %s'%resp_data
+    assert resp_data[0][0]["prio_classif"]["pred"][0] == "POMPIER",  'Not a fire fighter'
+    assert resp_data[0][0]["confidence"] > 0.7, "Car not detected"
+
 def test_predict_image(app, file_clio4):
 
     with app.test_client() as c:
@@ -37,3 +42,15 @@ def test_predict_url(app, url_clio):
     # Test with an image
     resp_data = eval(resp.get_data(as_text=True))
     assert_clio(resp_data)
+
+def test_predict_url(app, url_pompier):
+    with app.test_client() as c:
+        resp = c.post(
+            '/matchvec/predict',
+            content_type = 'multipart/form-data',
+            data = {'url':url_pompier})
+        print(resp.get_data(as_text=False)[0])
+
+    # Test with an image
+    resp_data = eval(resp.get_data(as_text=True))
+    assert_pompier(resp_data)
