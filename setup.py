af600dc10629f66b71844361760d3aaee82c2365
Laurent Guichard <laurent1.guichard@interieur.gouv.fr>
2021-10-15 09:38:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+from os import environ
+from os.path import join, abspath, dirname
 from setuptools import setup, find_packages
 
-setup(name="matchvec", packages=find_packages())
+root_path = abspath(dirname(__file__))
+
+with open(join(root_path, 'requirements.txt')) as f:
+    REQUIREMENTS = [line.strip() for line in f]
+
+setup(name="matchvec", 
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    entry_points={'console_scripts': [
+          'matchvecapp = matchvecapp.app:main'
+      ]},)
